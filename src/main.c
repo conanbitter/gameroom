e34f436ffdc599116524fc38e85d888d5fff4bcd
Conan Bitter <conan.bitter@gmail.com>
2022-10-31 21:48:01 +0300

    Improve window creation

--- src/main.c
+++ src/main.c
@@ -3,6 +3,11 @@
 #include <windows.h>
 #include <stdint.h>
 
+#define CLASS_NAME L"GameRoom"
+#define WIN_W 640
+#define WIN_H 395
+#define WIN_STYLE (WS_OVERLAPPED | WS_CAPTION | WS_SYSMENU | WS_MINIMIZEBOX)
+
 HBITMAP hBMP;
 
 void OnLoad() {
@@ -16,27 +21,49 @@ LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
 
 int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PWSTR pCmdLine, int nCmdShow) {
     // Register the window class.
-    const wchar_t CLASS_NAME[] = L"GameRoom";
 
-    WNDCLASS wc = {0};
+    WNDCLASSEX wc;
+    SIZE screenSize;
+    LONG winX, winY;
 
+    ZeroMemory(&wc, sizeof(WNDCLASSEX));
+
+    wc.cbSize = sizeof(WNDCLASSEX);
+    wc.hbrBackground = (HBRUSH)GetStockObject(WHITE_BRUSH);
     wc.lpfnWndProc = WindowProc;
     wc.hInstance = hInstance;
     wc.lpszClassName = CLASS_NAME;
+    wc.hIcon = LoadIcon(hInstance, L"icMain");
     wc.hCursor = LoadCursor(0, IDC_ARROW);
+    wc.style = CS_HREDRAW | CS_VREDRAW;
+    wc.cbWndExtra = 0;
+    wc.cbClsExtra = 0;
+    wc.lpszMenuName = NULL;
 
-    RegisterClass(&wc);
+    if (!RegisterClassEx(&wc)) {
+        return -1;
+    }
+
+    screenSize.cx = GetSystemMetrics(SM_CXSCREEN);
+    screenSize.cy = GetSystemMetrics(SM_CYSCREEN);
+
+    winX = (screenSize.cx - (WIN_W + GetSystemMetrics(SM_CXBORDER) * 2)) / 2;
+    winY = (screenSize.cy - (WIN_H + GetSystemMetrics(SM_CYBORDER) + GetSystemMetrics(SM_CYCAPTION))) / 2;
+
+    RECT winRect = {.left = winX, .top = winY, .right = winX + WIN_W, .bottom = winY + WIN_H};
+
+    AdjustWindowRectEx(&winRect, WIN_STYLE, FALSE, WS_EX_OVERLAPPEDWINDOW);
 
     // Create the window.
 
     HWND hwnd = CreateWindowEx(
-        0,                    // Optional window styles.
-        CLASS_NAME,           // Window class
-        L"GameRoom",          // Window text
-        WS_OVERLAPPEDWINDOW,  // Window style
+        WS_EX_OVERLAPPEDWINDOW,  // Optional window styles.
+        CLASS_NAME,              // Window class
+        L"Game Room",            // Window text
+        WIN_STYLE,               // Window style
 
         // Size and position
-        CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT,
+        winRect.left, winRect.top, winRect.right - winRect.left, winRect.bottom - winRect.top,
 
         NULL,       // Parent window
         NULL,       // Menu
@@ -45,7 +72,7 @@ int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PWSTR pCmdLine
     );
 
     if (hwnd == NULL) {
-        return 0;
+        return -1;
     }
 
     OnLoad();
@@ -56,11 +83,15 @@ int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PWSTR pCmdLine
 
     // Run the message loop.
 
-    MSG msg = {0};
+    MSG msg;
+    ZeroMemory(&msg, sizeof(MSG));
     while (GetMessage(&msg, NULL, 0, 0) > 0) {
         TranslateMessage(&msg);
         DispatchMessage(&msg);
     }
+
+    DestroyWindow(hwnd);
+    UnregisterClass(CLASS_NAME, hInstance);
 
     return 0;
 }
@@ -81,15 +112,16 @@ LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam) 
             // All painting occurs here, between BeginPaint and EndPaint.
 
             FillRect(hdc, &ps.rcPaint, (HBRUSH)(COLOR_WINDOW + 1));
-            TextOut(hdc, 0, 0, L"Hello, Windows! ", 15);
             hdcMem = CreateCompatibleDC(hdc);
             oldBitmap = SelectObject(hdcMem, hBMP);
 
             GetObject(hBMP, sizeof(bitmap), &bitmap);
-            BitBlt(hdc, 100, 100, bitmap.bmWidth, bitmap.bmHeight, hdcMem, 10, 10, SRCCOPY);
+            BitBlt(hdc, 0, 0, bitmap.bmWidth, bitmap.bmHeight, hdcMem, 0, 0, SRCCOPY);
 
             SelectObject(hdcMem, oldBitmap);
             DeleteDC(hdcMem);
+            SetBkMode(hdc, TRANSPARENT);
+            TextOut(hdc, 0, 0, L"Hello, Windows!", 15);
 
             OnDraw(hdc);
 
